1b2c6d398ce0b5f4d1223bce782a1ca737a206d4
ZaidRehmannn <zaidrehmann@gmail.com>
2025-03-04 15:36:12 +0500

    db issue resolved

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const connectDB = async () => {
-    await mongoose.connect(process.env.MONGO_DB_KEY).then(() => {
-        console.log('DB Connected');
-    })
-}
+    try {
+        await mongoose.connect(process.env.MONGO_DB_KEY);
+        console.log("Database Connected");
+    } catch (error) {
+        console.log("Database Connection Error:", error.message);
+    }
+};
